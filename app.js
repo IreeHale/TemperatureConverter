1ed6ae993dbb8128339a321dfbbb4f52912e4cf9
Iree Hale <KidFLash2099@protonmail.com>
2020-04-16 12:02:30 -0700

    working on javascript

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("hello world")
+const celciusInput = document.querySelector('#celcius > input');
+const fahrenheitInput = document.querySelector('#fahrenheit> input');
+const kelvinInput = document.querySelector('#kelvin > input');
+
+celciusInput.addEventListener('input', function(){
+    const cTemp = celciusInput.value;
+    const fTemp = (cTemp * (9/5)) + 32;
+});
